4998e29e61ed9bc51c09a36ff24e6655dbec8651
LeeSeongHa <toywwy@gmail.com>
2016-09-19 01:17:50 +0900

    여행가자_1976 유니온파인드이용해서 집합으로 나눠서 문제를 해결했다.

--- 알고리즘 준비 9월/여행가자_1976.cpp
+++ 알고리즘 준비 9월/여행가자_1976.cpp
@@ -2,58 +2,113 @@
 /*
 문제 : 여행가자
 문제 번호: 1976
-풀이법 : 다익스트라
 날짜 : 160918
-기타 : 
+기타 : union파인드, 크루스칼을 이용해서 스패닝트리 구하고나서 각각의 루트를 비교함
 */
 
 #include<iostream>
+#include<algorithm>
 
+#define N 200
 using namespace std;
 
-bool arr[202][202];
-int flag[202];
+
+class Edge
+{
+public:
+	int u, v;
+	Edge() : Edge(-1, -1) {}// default 생성자
+	Edge(int u, int v) :u(u), v(v){}// defalut 입력
+};
+
+
+int disjoint[N+2];
+
+int mFind(int a)
+{
+	if (disjoint[a] < 0)
+		return a;
+	else
+		return disjoint[a] = mFind(disjoint[a]);
+}
+
+bool mUnion(int a, int b)
+{
+	int _a = mFind(a);
+	int _b = mFind(b);
+
+	if (_a == _b)
+		return false;
+
+	disjoint[_b] = _a;
+	return true;
+}
 
 
 int main(void)
 {
 	int n, m;//도시수 200, 여행 계획에 속한 도시들 M개
-	//어려운게 반드시 되는게아니라 실패를 할수가있다는 거네..
 
 	cin >> n >> m;
 
+	Edge *edges = new Edge[n*n+2];
+
+	int input=0;
+	int edgeCnt = 0;
+
 	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> arr[i][j];
-
-	//일단 입력받고..
-	
-	//모든걸 다 방문 할수있는지 체크를 해야한다.
-	//여행 경로에는 중복해서 나온다.
-	//이런 부분의 하나하나 패스하는 방식으로 ...
-	//1000개의 패스를 받아야 한다는 말이다.
-	//flag를 두고 실행해보자...
-
-	int q;
-	for (int i = 0; i < m; i++)
 	{
-		scanf("%d", &q);
-		//문제는 경유지로 가기 위해서 경로에 없는 여행 경로를 선택 할 수도 있다.
-		//이게 어려운 문제네 ;;;
-		//바로갈수 있는게 없을떄 다른걸 선택한다 이런느낌으로 해야하는건가??
+		for (int j = 1; j <= n; j++)
+		{
+			scanf("%d", &input);
 
-
-
+			if (input != 0)
+			{
+				Edge *e = new Edge(i, j);
+				edges[edgeCnt] = *e;
+				edgeCnt++;
+			}
+		}
 	}
 
-	
+	fill(disjoint, disjoint+ N+2 , -1);
 
-	//마지막 여행 계획을 받고나서 그 계획대로 갈수있는지를 확인한다.
+	int value = 0;
+	int cnt = 1;
 
-	
+	//스패닝 트리를 못만들수있음. 그래서 <edgeCnt까지만!
+	for (int i = 0;i<edgeCnt; i++)
+	{
+		Edge e = edges[i];
 
+		if (mUnion(e.u, e.v))
+		{
+			if ((++cnt) >= n)
+				break;
+		}
+	}
 
+	//이미 스패닝 트리는 만들어진 상태. 스패닝 트리라고 하긴 그렇고 집합들이 만들어 졌겠지.
+	int q;
+	cin >> q;
 
+	int root = mFind(q);
+	bool res = true;
+
+	for (int i = 0; i < m-1; i++)
+	{
+		scanf("%d", &q);
+		//부모가 같나 확인한다.
+		if (root != mFind(q)) {
+			res = false;
+			break;
+		}
+	}
+
+	if (res)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
 
 	return 0;
 }
