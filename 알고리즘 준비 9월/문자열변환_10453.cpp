160eee8258648404f1d77da47f5e3777cdad7c7f
LeeSeongHa <toywwy@gmail.com>
2016-11-16 16:36:59 +0900

    문자열변환_10453

---
+++ 알고리즘 준비 9월/문자열변환_10453.cpp
@@ -0,0 +1,56 @@
+/*
+문제 : 문자열변환
+문제 번호: 10453
+풀이법 : 그냥 탐색
+날짜 : 161116
+기타 : 다른 부분을 캐치 하고 뒷부분 하고 교환을 해주는 그리고 그다음 부터 다시 탐색하는 방식
+
+*/
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+	int itr;
+	cin >> itr;
+
+	string src, dst;
+
+
+	for (int i = 0; i < itr; i++)
+	{
+		int res = 0;
+
+		cin >> src >> dst;
+
+		if (src.size() != dst.size())
+		{
+			cout << -1 << endl;
+			continue;
+		}
+
+		for (int j = 0; j < src.size(); j++)
+		{
+			if (src[j] != dst[j])
+			{
+				for (int k = j + 1; k < src.size(); k++)
+				{
+					res += 1;
+					if (src[j] != src[k])
+					{
+						swap(src[j], src[k]);
+						break;
+					}
+
+				}
+			}
+		}
+		cout << res << endl;
+	}
+
+
+	return 0;
+}
